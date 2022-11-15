9aa6b0cd29781731ae7a83ad3c3aac244a8686ff
unknown <austin.hall1215@gmail.com>
2022-11-15 14:37:01 -0600

    new background

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _speed = 5f;
     private Animator _animator;
     [SerializeField] private float _jumpSpeed = 5f;
+    private Collider2D _collider;
+    [SerializeField] private LayerMask _platformsLayerMask;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
         _playerRigidBody = GetComponent<Rigidbody2D>();
+        _collider = transform.GetComponent<Collider2D>();
+        
+
     }
 
     // Update is called once per frame
@@ -33,18 +38,22 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _playerRigidBody.velocity += new Vector2(0f, _jumpSpeed);
-           
-            _animator.SetBool("isSpaceBarActive", true);
+       
 
-        }
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            _animator.SetBool("isSpaceBarActive", false);
-        }
+            if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+            {
+                _playerRigidBody.velocity += new Vector2(0f, _jumpSpeed);
+
+                _animator.SetBool("isSpaceBarActive", true);
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                _animator.SetBool("isSpaceBarActive", false);
+            }
+        
          
     }
 
@@ -68,5 +77,11 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(_playerRigidBody.velocity.x), 1f);
         }
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d= Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size, 0f, Vector2.down, .1f, _platformsLayerMask);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d != false;
     }
 }
---
+++ Assets/Scroll.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * _speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
